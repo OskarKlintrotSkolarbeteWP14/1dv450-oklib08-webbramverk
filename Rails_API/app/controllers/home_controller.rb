d0ffaf0d8987cca51e61c4b1e18c6cf427e78477
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-02 12:43:53 +0100

    Added a layout and a new view

--- Rails_API/app/controllers/home_controller.rb
+++ Rails_API/app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def new
+    @user = User.new
+  end
 end
--- Rails_API/config/routes.rb
+++ Rails_API/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :home
 
   # Example resource route with options:
   #   resources :products do
