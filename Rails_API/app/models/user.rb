1f2fe3127a90a4bf1f114e53ca00f7c8c32893f8
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-03 15:57:53 +0100

    Added validation

--- Rails_API/app/models/user.rb
+++ Rails_API/app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
   has_many :applications
 
-  validates :username, presence: true,
-    length: { minimum: 3, maximum: 20 },
-    uniqueness: { case_sensitive: false }
-  validates :password, presence: true,
-    length: { minimum: 6, maximum: 50 },
+  validates :username, presence: { message: "Du måste ange ett användarnamn!" },
+    length: { minimum: 3, maximum: 20, message: "Användarnamnet måste vara minst 3 tecken och max 20 tecken." },
+    uniqueness: { case_sensitive: false, message: "Användarnamnet är upptaget." }
+  validates :password, presence: { message: "Du måste ange ett lösenord!" },
+    length: { minimum: 6, message: "Lösenordet måste vara minst 6 tecken." },
     confirmation: true
-  validates :first_name, length: { minimum: 2, maximum: 20 }
-  validates :last_name, length: { minimum: 2, maximum: 30 }
+  validates :first_name, length: { minimum: 2, maximum: 20, message: "Förnamnet måste vara minst 2 tecken och max 20 tecken." }
+  validates :last_name, length: { minimum: 2, maximum: 30, message: "Efternamnet måste vara minst 2 tecken och max 20 tecken." }
 
   has_secure_password
 end
