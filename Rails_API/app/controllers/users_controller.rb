ea218467d0b2e5a2efba89c3de532870aed34fbb
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-03 14:07:47 +0100

    Registration works

--- Rails_API/app/controllers/users_controller.rb
+++ Rails_API/app/controllers/users_controller.rb
@@ -3,14 +3,30 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+
+  end
+
   def new
     @user = User.new
   end
 
   def create
+    @user = User.new(link_params)
 
+    if @user.save
+      redirect_to users_path
+    else
+      render :action => "new"
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def link_params
+    params.require(:user).permit(:username, :password, :first_name, :last_name)
   end
 end
