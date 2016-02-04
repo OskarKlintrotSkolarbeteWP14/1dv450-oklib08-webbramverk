b8d20eb609e336597e91c710f27aa6afd2250b66
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-04 16:13:45 +0100

    Cleaned up the code for generating the key

--- Rails_API/app/helpers/keys_helper.rb
+++ Rails_API/app/helpers/keys_helper.rb
@@ -1,10 +1,7 @@
 module KeysHelper
   def generate_key
-    self.api_key = SecureRandom.hex
-
-    # Make sure the key has not been used before
-    while Key.find_by_api_key(self.api_key) do
+    begin
       self.api_key = SecureRandom.hex
-    end
+    end while self.class.exists? api_key: api_key
   end
 end
