6bee4a27d179a9ff2c4cee53d8c5aa25580d76e5
OskarKlintrot <oskar.klintrot@gmail.com>
2016-03-27 00:33:21 +0100

    Removed comments

--- angularApp/client/app/scripts/factory/opsTableFactory.js
+++ angularApp/client/app/scripts/factory/opsTableFactory.js
@@ -3,11 +3,6 @@ angular.module(C.appName).factory('OpsTable', function(
   NgTableParams
 ){
   return function(options) {
-    // var options = options || {},
-    //   query = options.query || null,
-    //   force = options.force || null,
-    //   url = options.url || 'ops'
-
     return new NgTableParams({}, {
       getData: function(params) {
         return Resources.getNewOps(options).then(function(data) {
