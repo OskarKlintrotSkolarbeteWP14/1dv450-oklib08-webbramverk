63f1b785894b3cef34d5bf755ca296edfaaf2198
OskarKlintrot <oskar.klintrot@gmail.com>
2016-03-26 15:12:30 +0100

    Refactored positions resources to own factory

---
+++ angularApp/client/app/scripts/factory/positionsResourcesFactory.js
@@ -0,0 +1,84 @@
+angular.module(C.appName).factory('PositionsResources', function($q, Restangular){
+  var self = this // Lovely old school ES5 hack!
+
+  function getPositionsForOps(ops) {
+    return $q(function(resolve){
+      var promises = []
+      ops.forEach(function(op, index, array){
+        promises.push(op.one('positions').get())
+        if (promises.length === array.length) {
+          resolve({
+            ops: ops,
+            promises: promises
+          })
+        }
+      })
+    })
+  }
+
+  function resolvePromisesForPositions(data) {
+    return $q(function(resolve){
+      $q.all(data.promises)
+        .then(function(positions){
+          resolve ({
+            ops: data.ops,
+            positions: _.uniqBy(positions, 'position.id')
+          })
+        })
+    })
+  }
+
+  function mergeOpsWithPosition(data) {
+    var ops = []
+    data.ops.forEach(function(op){
+      data.positions.forEach(function(pos){
+        if(pos.position.id === op.position.id){
+          ops.push(
+            _.assign(
+              {}, op, {
+                position: _.assign({}, op.position, pos.position)
+              }
+            )
+          )
+        }
+      })
+    })
+    return ops
+  }
+
+  function getAll(resolve, query, force) {
+    if(!self.data || query || force){
+      console.log('Fetching new data')
+      var promise = Restangular
+        .all('ops')
+        .getList({search: query})
+        .then(function(data){
+          return getPositionsForOps(data)
+        })
+        .then(function(data){
+          return resolvePromisesForPositions(data)
+        })
+        .then(function(data){
+          return mergeOpsWithPosition(data)
+        })
+        .then(function(data){
+          self.data = data
+          resolve(data)
+        })
+    } else {
+      console.log('Cached data')
+      resolve(self.data)
+    }
+  }
+
+  return {
+    getCurrentData: function() {
+      return self.data
+    },
+    getNewData: function(query, force) {
+      return $q(function(resolve){
+        getAll(resolve, query, force)
+      })
+    }
+  }
+})
